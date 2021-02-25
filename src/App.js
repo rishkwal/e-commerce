dba5767d9370d452067062523d77ada381530fe9
Rishabh Khandelwal <rishabhk2512@gmail.com>
2021-02-25 16:51:58 +0530

    Add To Cart configured

--- src/App.js
+++ src/App.js
@@ -19,6 +19,12 @@ function App() {
        
     }
 
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+    }
+
     useEffect(() => {
         fetchProducts();
         fetchCart();
@@ -30,7 +36,7 @@ function App() {
     return (
         <div>
             <Navbar/>
-            <Products products={products} />
+            <Products products={products} onAddToCart={handleAddToCart} />
         </div>
     )
 }
--- src/Components/Products/Product/Product.jsx
+++ src/Components/Products/Product/Product.jsx
@@ -3,7 +3,7 @@ import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from
 import { AddShoppingCart } from '@material-ui/icons';
 import useStyles from './styles';
 
-function Product({product}) {
+function Product({product, onAddToCart}) {
     const classes = useStyles();
 
 
@@ -23,7 +23,7 @@ function Product({product}) {
                 <Typography dangerouslySetInnerHTML={{ __html: product.description}} variant='body2' color='textSecondary'/>
             </CardContent>
             <CardActions disableSpacing clasName = {classes.cardActions}>
-                <IconButton aria-label="Add to Cart">
+                <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
                     <AddShoppingCart />
                 </IconButton>
             </CardActions>
