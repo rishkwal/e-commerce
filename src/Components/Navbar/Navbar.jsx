fe8d9d477ea72fa1401ec35e00d9bf8a2ebc8c8a
Rishabh Khandelwal <rishabhk2512@gmail.com>
2021-03-02 12:20:19 +0530

    Links added to nav Bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import {AppBar, Toolbar, IconButton, Badge, Menuitem, Menu, Typography} from '@m
 import {ShoppingCart} from '@material-ui/icons';
 import logo from '../../assets/color_logo_transparent.png';
 import useStyles from './styles';
+import { Link } from 'react-router-dom';
 
 
 const Navbar = ({totalItems}) => {
@@ -11,13 +12,13 @@ const Navbar = ({totalItems}) => {
         
         <AppBar position ='fixed' className={classes.appBar} color='inherit'>
             <Toolbar>
-                <Typography variant='h6' className={classes.title} color='inherit'>
+                <Typography component={Link} to='/' variant='h6' className={classes.title} color='inherit'>
                     <img src={logo} alt ='RishKart.js' height ='25px' className={classes.image} />
                     RishKart
                 </Typography> 
                 <div className = {classes.grow} />
                 <div className={classes.button}>
-                    <IconButton aria-label='Show Cart Items' color = 'inherit'>
+                    <IconButton component={Link} to='/cart' aria-label='Show Cart Items' color = 'inherit'>
                         <Badge badgeContent={totalItems} color='secondary'>
                             <ShoppingCart/>
                         </Badge>
