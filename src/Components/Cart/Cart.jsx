6d098a1a07f1910138de22e2d84ff43d38e5f11c
Rishabh Khandelwal <rishabhk2512@gmail.com>
2021-03-02 12:36:03 +0530

    deploying new build

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Typography, Button, Grid} from '@material-ui/core';
 import CartItem from './CartItem/CartItem'
+import { Link } from 'react-router-dom';
 
 import useStyles from './styles';
 
@@ -9,7 +10,9 @@ const Cart = ({ cart }) => {
     const classes = useStyles();
 
     const EmptyCart =() => {return(
-        <Typography variant='subtitle1'>You have no items in your shopping cart, start adding some!</Typography>
+        <Typography variant='subtitle1'>You have no items in your shopping cart, 
+            <Link to='/' className={classes.link}>start adding some!</Link>
+        </Typography>
     )
     };
 
