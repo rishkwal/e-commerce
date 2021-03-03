507cbfa82e3c6e257e79e50850dfab19a49440da
Rishabh Khandelwal <rishabhk2512@gmail.com>
2021-03-03 17:36:20 +0530

    cart buttons working

--- src/Components/Cart/CartItem/CartItem.jsx
+++ src/Components/Cart/CartItem/CartItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Typography, Button, Card, CardActions, CardContent, CardMedia} from '@material-ui/core';
 import useStyles from './styles';
 
-function CartItem({ item }) {
+function CartItem({ item, handleUpdateCartQty,handleRemoveFromCart, handleEmptyCart }) {
     const classes = useStyles();
     return (
         <Card>
@@ -13,11 +13,11 @@ function CartItem({ item }) {
             </CardContent>
         <CardActions className={classes.cardActions}>
             <div className={classes.buttons}>
-                <Button type='button' size='small'>-</Button>
+                <Button type='button' size='small' onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
                 <Typography>{item.quantity}</Typography>
-                <Button type='button' size='small'>+</Button>
+                <Button type='button' size='small' onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}>+</Button>
             </div>
-        <Button variant='contained' type='button' color='secondary'>Remove</Button>
+        <Button variant='contained' type='button' color='secondary' onClick={() => handleRemoveFromCart(item.id)}>Remove</Button>
         </CardActions>
         </Card>
             
